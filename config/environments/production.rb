53a8da44063baf266983f8a2b59521ad7025fa4c
Prelang Builder <builder@prelang.com>
2017-05-23 18:00:35 +0000

    Injected Google Analytics tracker ID configuration into production configuration

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,4 +79,9 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  #->Prelang
+  GA.tracker = "UA-00000000-1"
+
 end
+
